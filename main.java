47e6d0c2fc0199bba57c8e73f1e8f6cbcd6aa52a
shyammohanbhat <131670308+shyammohanbhat@users.noreply.github.com>
2024-08-31 15:16:02 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,11 @@
-publ stac void() {} 
+public class Main {
+    public static void main(String[] args) {
+        Strin str1 = "Hello, "
+        String str2 = "World!";
+        
+        String result = str1 + str2;
+        
+        System.out.println(result);
+    }
+}
+ 
